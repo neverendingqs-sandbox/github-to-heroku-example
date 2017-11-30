7be3bf5cc0aefc2bddc5d704ab0e0ce3b5478ccb
Mark Tse <mark.tse@neverendingqs.com>
2017-11-30 15:03:25 -0500

    chore - updating versions of dependencies.

--- src/server.js
+++ src/server.js
@@ -1,13 +1,5 @@
-const express = require('express');
+const app = require('./app.js');
 const port = process.env.PORT || 3000;
-
-const app = express();
-
-app.get('/', function(req, res) {
-  res.send("<h1>Hello World</h1>");
-});
 
 app.listen(port);
 console.log(`HTTP started on port ${port}.`);
-
-module.exports = app;
